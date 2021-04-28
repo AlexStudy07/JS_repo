237ffb17b3f4da0f76ebbf06f86be1d4cdac3610
AlexStudy07 <83111825+AlexStudy07@users.noreply.github.com>
2021-04-28 16:16:03 +0300

    Update app_8Ball.js
    Added comments

--- app_8Ball.js
+++ app_8Ball.js
@@ -1,28 +1,28 @@
 var playerName; //variable for name from HTML input
 var checkBtn; // buttin for name check
-var userQuescion = '';
-var randomNumber = Math.floor(Math.random() * 8);
-var eightBall = '';
-function getNameFromInput() {
+var userQuescion = ''; // variable for user quescion
+var randomNumber = Math.floor(Math.random() * 8); //Random Number
+var eightBall = ''; //variable for quescion identification by random number
+function getNameFromInput()/*used for name extraction from HTML input */ {
     playerName = document.getElementById('Name').value;
     playerName ? console.log(`Hello, ${playerName}!`) : console.log('Hello guest!');
-    document.getElementById("yBtn").disabled = false;
-    document.getElementById("nBtn").disabled = false;
+    document.getElementById("yBtn").disabled = false; //yes button activation for future actions
+    document.getElementById("nBtn").disabled = false; //no button activation for future actions
 }
-function noAnswer() {
+function noAnswer() /*actions if no button is clicked*/ {
     console.log('Good Bye');
     document.getElementById("yBtn").disabled = true;
 }
-function activateQuescionField() {
+function activateQuescionField() /*actions if yes button is clicked*/ {
     document.getElementById("quescion").disabled = false;
     
     document.getElementById("nBtn").disabled = true;
 }
-function getQuescionFromInput() {
+function getQuescionFromInput() /*check if quescion field is not empty*/ {
     userQuescion = document.getElementById('quescion').value;
     userQuescion ? document.getElementById("ckQuescionBtn").disabled = false : console.log('Quescion field is empty');
 }
-function checkQuescion() {
+function checkQuescion() /*Get answer for answer on Check Quescion button click*/{
 var randomNumber = Math.floor(Math.random() * 8);
 eightBall = randomNumber;
 switch(eightBall) {
